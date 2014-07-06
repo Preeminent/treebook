a2a3115ba3b2d9a08f91ef6d8195421f8c6cecd6
Anthony <anthony@preeminentproductions.com>
2014-07-06 16:56:56 -0700

    refactor Flash Message code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def flash_class(type)
+		case type
+  			when 'alert' then " alert alert-danger"
+  			when 'notice' then "alert alert-success"
+  	else
+  		""
+  	end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 has_many :statuses
 
+
+validates :first_name, presence: true
+validates :last_name, presence: true
+validates :profile_name, presence: true, uniqueness: true
+
+
 	def full_name
 
 	 first_name + " " + last_name
