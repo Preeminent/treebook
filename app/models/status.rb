b79486c246bf38618882e38fe4d9767c266eff5b
Anthony <anthony@preeminentproductions.com>
2014-07-07 16:43:12 -0700

    add validation before posting

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,8 @@
 class Status < ActiveRecord::Base
 	belongs_to :user
+
+	validates :content, presence: true,
+	                          length: { minumum: 2}
+
+	 validates :user_id, presence: true
 end
